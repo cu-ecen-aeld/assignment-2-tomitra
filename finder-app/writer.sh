707efef80d1e21e68fbd3724768d31ca43824a45
tomitra <tojmbb@gmail.com>
2023-04-20 16:58:00 +0100

    done1

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,17 +1,39 @@
 #!/bin/bash
 
-#   Check if arguments are provided
-if [ $# -ne 2 ]; then
-    echo "Error: Required arguments not provided."
-    exit 1
+# Check if the number of arguments is correct
+if [ "$#" -ne 2 ]; then
+  echo "Error: Incorrect number of arguments."
+  echo "Usage: $0 <writefile> <writestr>"
+  exit 1
 fi
 
-# Create the directory if it doesn't exist
-dir=$(dirname "$1")
-mkdir -p "$dir"
+# Extract arguments
+writefile="$1"
+writestr="$2"
 
-# Write to the file
-if ! echo "$2" > "$1"; then
-    echo "Error: Unable to write to file."
-    exit 1
+# Check if writefile argument is provided
+if [ -z "$writefile" ]; then
+  echo "Error: 'writefile' argument not specified."
+  exit 1
 fi
+
+# Check if writestr argument is provided
+if [ -z "$writestr" ]; then
+  echo "Error: 'writestr' argument not specified."
+  exit 1
+fi
+
+# Create directory if it doesn't exist
+mkdir -p "$(dirname "$writefile")"
+
+# Write content to file
+echo "$writestr" > "$writefile"
+
+# Check if file was created successfully
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to create file '$writefile'"
+  exit 1
+fi
+
+# Exit with success status
+exit 0
